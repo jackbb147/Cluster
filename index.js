85273133534510d43f74b9239268ebf4551a9477
Jack Hou <jackhou147@berkeley.edu>
2022-07-19 23:12:36 -0700

    some refactoring

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const helpers = require("./helperMethods");
 const print = helpers.print;
+const query = require("./controller/db.js").query;
 const mysql = require('mysql2/promise');
 const config = require('./config.js').config;
+const connect = require("./controller/db").connect;
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 1700;
+const port = 1700;
 const TABLENAMES = ["feedback_clusters", "feedback_sentences", "sentence_cluster_mapping"]
 app.use(express.json());
 app.use(
@@ -14,31 +16,20 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => {
-    res.send("hello!")
-});
-
-async function main(){
-
-    print("mysql connection established: ", connection);
-}
 
 
 
-async function query(sql, params) {
 
-    const [results, ] = await connection.execute(sql, params);
-    return results;
-}
 
 app.listen(port, async () => {
-    const tables = TABLENAMES;
-    const query =  `SELECT * FROM ${tables[0]}`;
-    print("line 34: hello world!", query);
-    const connection = await mysql.createConnection(config.db);
-    const results = await connection.execute(
-        query
-    )
-    print(results);
-
+    print(`APP LISTENING ON PORT ${port}`)
 })
+
+app.get("/", async (req, res) => {
+    const tables = TABLENAMES;
+    const qStr =  `SELECT * FROM ${tables[0]}`;
+
+
+    const results = await query(qStr)
+    res.json(results);
+});
